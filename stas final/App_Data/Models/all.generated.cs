bb506efcbebb0d19feb5907ebb2d95551342ee9e
rheo <rheo.matabadal.natin@gmail.com>
2018-07-06 12:39:01 -0300

    blokjes worden al verrtoond

--- stas final/App_Data/Models/all.generated.cs
+++ stas final/App_Data/Models/all.generated.cs
@@ -9,7 +9,7 @@ using  Umbraco.ModelsBuilder;
 using  Umbraco.ModelsBuilder.Umbraco;
 [assembly: PureLiveAssembly]
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "24e37bbc3085ce92")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.2")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.3")]
 
 
 // FILE: models.generated.cs
