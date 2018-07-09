63d0b13c10a64e3d4b40972589d78f80dca5d10b
rheo <rheo.matabadal.natin@gmail.com>
2018-07-09 16:01:27 -0300

    contact form check for null values

--- stas final/App_Data/Models/all.generated.cs
+++ stas final/App_Data/Models/all.generated.cs
@@ -9,7 +9,7 @@ using  Umbraco.ModelsBuilder;
 using  Umbraco.ModelsBuilder.Umbraco;
 [assembly: PureLiveAssembly]
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "1f9218be9ec51256")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.3")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
 
 
 // FILE: models.generated.cs
