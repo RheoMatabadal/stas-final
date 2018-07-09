4bb110394d5678156aaff8e275d3fec9e5c1b138
rheo <rheo.matabadal.natin@gmail.com>
2018-07-09 10:07:11 -0300

    artist slider recursive

--- stas final/App_Data/Models/all.generated.cs
+++ stas final/App_Data/Models/all.generated.cs
@@ -9,7 +9,7 @@ using  Umbraco.ModelsBuilder;
 using  Umbraco.ModelsBuilder.Umbraco;
 [assembly: PureLiveAssembly]
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "1f9218be9ec51256")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.2")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.3")]
 
 
 // FILE: models.generated.cs
--- stas final/App_Data/Models/models.generated.cs
+++ stas final/App_Data/Models/models.generated.cs
@@ -20,7 +20,7 @@ using Umbraco.ModelsBuilder.Umbraco;
 
 [assembly: PureLiveAssembly]
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "1f9218be9ec51256")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.2")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.3")]
 
 namespace Umbraco.Web.PublishedContentModels
 {
